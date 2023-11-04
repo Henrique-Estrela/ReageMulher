e0045c9241340a2963f084374bcbb5fa077496a1
Henrique Estrela <92654861+Henrique-Estrela@users.noreply.github.com>
2023-11-04 16:52:13 -0300

    Up

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,25 +1,33 @@
 var banner_quiz = document.querySelector("#main-banner-quiz");
+var btn_next = document.querySelector("#quiz-button-next");
+var quiz = [1, 2, 3, 4, 5, 6, 7];
+
+var i = 0;
+
+function next_query() {  
+    if (i < quiz.length) {
+        if (i > 0) {
+            document.getElementById("query" + quiz[i - 1]).style.display = 'none';
+        }
+        document.getElementById("query" + quiz[i]).style.display = 'block';
+        i++
+    } else {
+        btn_next.style.display = 'none';
+    }
+
+}
 
 function Open_close_query() {
     banner_quiz.style.display = 'none';
-    let quiz = ["1","2","3","4","5","6","7"];
-    for (let i = 0; i < quiz.length; i++) {
-        document.getElementById("query"+quiz[i]).style.display = 'block';
-        if (indiceConteudoAtual < conteudos.length) {
-            conteudos[indiceConteudoAtual].style.display = "none";
-            indiceConteudoAtual++;
-        }
-    
-        // Mostra o próximo conteúdo, se disponível
-        if (indiceConteudoAtual < conteudos.length) {
-            conteudos[indiceConteudoAtual].style.display = "block";
-        } else {
-            botaoProximo.disabled = true; // Desativa o botão quando todos os conteúdos foram exibidos
-        }
-    }
+    btn_next.style.display = 'block';
+
+    next_query()
 }
 
+document.getElementById("quiz-button-next").addEventListener('click', function () {
+    next_query()
+});
 
-document.getElementById("quiz-button-start").addEventListener('click', function start() {
+document.getElementById("quiz-button-start").addEventListener('click', function () {
     Open_close_query();
 });
